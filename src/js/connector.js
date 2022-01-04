a6355f18e9e2f627634228b9b7c20cf8fe5b0e09
Oleg Kalyta <oleg@productcrafters.io>
2022-01-04 12:17:32 +0200

    another capability

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,7 +1,10 @@
-console.log("I'm here");
+console.log('hello hello');
 
 window.TrelloPowerUp.initialize({
-  "card-badges": function (t, opts) {
-    return [];
+  'card-buttons': function(t, options){
+    return [{
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      text: 'Estimate Size',
+    }];
   },
 });
