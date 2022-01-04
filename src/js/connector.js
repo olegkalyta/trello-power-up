5ec03697356b46d5c037f83af365b014f2e3ff58
Oleg Kalyta <oleg@productcrafters.io>
2022-01-04 11:17:22 +0200

    added power-up boilerplate code & nvmrc

---
+++ src/js/connector.js
@@ -0,0 +1,10 @@
+console.log("I'm here");
+
+window.TrelloPowerUp.initialize({
+  "card-badges": function (t, opts) {
+    return [];
+  },
+}, {
+  appKey: 'd26f90d692b09eaa03aec3c2373d4dd8',
+  appName: 'Better Time in Status'
+});
