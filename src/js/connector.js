83506dd1ae1e4b86a2a0e1095b1938996e518049
Oleg Kalyta <oleg@productcrafters.io>
2022-01-04 11:25:32 +0200

    removed auth

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,7 +4,4 @@ window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     return [];
   },
-}, {
-  appKey: 'd26f90d692b09eaa03aec3c2373d4dd8',
-  appName: 'Better Time in Status'
 });
